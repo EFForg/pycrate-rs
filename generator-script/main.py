9b5a3ee50a946b514627ab8350678112a0ed0852
Will Greenberg <willg@eff.org>
2025-03-20 12:52:54 -0700

    add IMSI identity request test case

--- generator-script/main.py
+++ generator-script/main.py
@@ -64,6 +64,7 @@ def main(output_filepath: str, pcap_dir_filepath: str):
     emm_classes = list(NASLTE.EMMTypeMOClasses.values())
     emm_classes.append(NASLTE.EMMTypeMTClasses[69])  # add in the MT version of DetachRequest
     generate_module(os.path.join(output_filepath, 'emm'), emm_classes, [
+        '075501', # EMM IMSI identity request
         '0748610bf602f8108003c8c2e65e9a5804e060c0405202f810c4c25c0a00570220003103e5e0341302f810040511035758a65d0100c1', # EMM TAU Request
         '075e23093395684292874145f0', # EMM SMCompl
         '074300035200c2', # EMM Attach Complete
